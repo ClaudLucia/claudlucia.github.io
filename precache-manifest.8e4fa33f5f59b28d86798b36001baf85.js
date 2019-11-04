903499498f2e7b71ad8ccd4dac3ed6266306817f
ClaudLucia <claudia.rojas1@marist.edu>
2019-11-04 16:19:04 -0500

    updated files

---
+++ precache-manifest.8e4fa33f5f59b28d86798b36001baf85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbdbb62c118e9c2dcaf9c3413691aea9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21b698d55f3d837132e1",
+    "url": "/static/css/main.ee493363.chunk.css"
+  },
+  {
+    "revision": "7fe5e1c0ae9b1c4bc926",
+    "url": "/static/js/2.37401ba2.chunk.js"
+  },
+  {
+    "revision": "21b698d55f3d837132e1",
+    "url": "/static/js/main.7236c427.chunk.js"
+  },
+  {
+    "revision": "6c238ac8f6ef9860343f",
+    "url": "/static/js/runtime-main.ef219979.js"
+  }
+]);
