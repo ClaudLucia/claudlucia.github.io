5a70ab4534b0ef761a091044856a46a294e1127f
Claudia Rojas <52762774+ClaudLucia36@users.noreply.github.com>
2019-11-04 16:23:24 -0500

    Updates

--- precache-manifest.419076c2eb00e893c92385bae8e712c4.js
+++ precache-manifest.419076c2eb00e893c92385bae8e712c4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fbdbb62c118e9c2dcaf9c3413691aea9",
+    "revision": "54fb69b9d54c1685dda6c891d14029e2",
     "url": "/index.html"
   },
   {
-    "revision": "21b698d55f3d837132e1",
+    "revision": "3d721e2b27789c957a33",
     "url": "/static/css/main.ee493363.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.37401ba2.chunk.js"
   },
   {
-    "revision": "21b698d55f3d837132e1",
-    "url": "/static/js/main.7236c427.chunk.js"
+    "revision": "3d721e2b27789c957a33",
+    "url": "/static/js/main.fcbaee89.chunk.js"
   },
   {
     "revision": "6c238ac8f6ef9860343f",
